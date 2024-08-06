7e594ced458a28f98e788b55f4de62e0e01d7526
Paulo-xavier-a <pr3546287@gmail.com>
2024-08-06 20:38:48 -0300

    utlizando estilo global

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Details} from './pages/Details/Index.jsx'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './pages/styles/global.js'
+
+import theme from './pages/styles/theme.js'
+
+import { Details } from './pages/Details/Index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>,
 )
