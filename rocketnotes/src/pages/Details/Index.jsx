582c440c635e9d89bb81ef82ab87d0130fbc38a6
Paulo-xavier-a <pr3546287@gmail.com>
2024-08-06 21:22:37 -0300

    criando button

--- rocketnotes/src/pages/Details/Index.jsx
+++ rocketnotes/src/pages/Details/Index.jsx
@@ -1,10 +1,12 @@
  import { Container } from "./styles"
+ import { Button } from "../../components/Button/index.jsx"
 
  export function Details(){
   return(
     <Container>
 
     <h1>Hello world</h1>
+    <Button/>
     </Container>
   )
 }
